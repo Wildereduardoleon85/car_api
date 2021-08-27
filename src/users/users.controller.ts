32e922311553a12df0214f8cdd35bfa9fb8fbebb
Wilder León <wilder_eduardo85@hotmail.com>
2021-08-27 12:10:27 -0400

    remove user route added to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,7 +5,8 @@ import {
     Get,
     Patch,
     Param,
-    Query
+    Query,
+    Delete
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -27,5 +28,10 @@ export class UsersController {
     @Get()
     findAllUsers(@Query('email') email:string){
         return this.usersService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id:string){
+        return this.usersService.remove(parseInt(id))
     }
 }
